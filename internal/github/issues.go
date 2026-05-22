83c0a1fb902338e3091e7b373c08d503bd403379
Bulletguitarist <jyotirmoymahapatra108@gmail.com>
2026-05-22 19:27:25 +0530

    fix: add pagination to GetIssues() and expand Issue struct

--- internal/github/issues.go
+++ internal/github/issues.go
@@ -1,12 +1,37 @@
 package github
 
+import (
+	"fmt"
+	"time"
+)
+
 type Issue struct {
-	State string `json:"state"`
+	Number    int        `json:"number"`
+	State     string     `json:"state"`
+	CreatedAt time.Time  `json:"created_at"`
+	ClosedAt  *time.Time `json:"closed_at"`
+	PullRequest *struct{} `json:"pull_request"`
 }
 
 func (c *Client) GetIssues(owner, repo string, state string) ([]Issue, error) {
-	var issues []Issue
-	url := "https://api.github.com/repos/" + owner + "/" + repo + "/issues?state=" + state
-	err := c.get(url, &issues)
-	return issues, err
+	var all []Issue
+	page := 1
+	perPage := 100
+
+	for {
+		url := fmt.Sprintf(
+			"https://api.github.com/repos/%s/%s/issues?state=%s&per_page=%d&page=%d",
+			owner, repo, state, perPage, page,
+		)
+		var batch []Issue
+		if err := c.get(url, &batch); err != nil {
+			return nil, err
+		}
+		all = append(all, batch...)
+		if len(batch) < perPage {
+			break
+		}
+		page++
+	}
+	return all, nil
 }
